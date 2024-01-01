acfd97f08878961f96d8f46171c91f005c73b91b
Naomi Mogi <nyabokenaomi001@gmail.com>
2024-01-02 02:49:49 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const tasksList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const theDescription = document.getElementById("new-task-description");
+    const taskDescriptionInput = theDescription.value;
+
+    const newActivity = document.createElement("li");
+    newActivity.textContent = taskDescriptionInput;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "❌";
+    deleteButton.addEventListener("click", function () {
+      tasksList.removeChild(newActivity);
+    });
+
+    newActivity.appendChild(deleteButton);
+
+    tasksList.appendChild(newActivity);
+
+    theDescription.value = "";
+  });
 });
